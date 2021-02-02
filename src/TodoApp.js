07b065a43715e721f007a9685014edfc58cbfbc2
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-02-01 19:14:25 -0800

    LocalStorage with UseEffect Hook

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Typography from "@material-ui/core/Typography"
 import Paper from "@material-ui/core/Paper"
 import AppBar from "@material-ui/core/AppBar"
@@ -9,12 +9,14 @@ import TodoForm from './TodoForm'
 import { v4 as uuidv4 } from 'uuid';
 
 function TodoApp() {
-    const initialTodos = [
-        { id: 1, task: "Drink more water", completed: false },
-        { id: 2, task: "Walk Ollie", completed: true },
-        { id: 3, task: "Make Dinner", completed: false }
-    ]
+    const initialTodos = JSON.parse(window.localStorage.getItem('todos') || "[]")
+
     const [todos, setTodos] = useState(initialTodos)
+
+    useEffect(() => {
+        window.localStorage.setItem("todos", JSON.stringify(todos))
+    }, [todos]);
+
     const addTodo = newTodoText => {
         setTodos([...todos, { id: uuidv4(), task: newTodoText, completed: false }])
     }
--- src/TodoList.js
+++ src/TodoList.js
@@ -6,26 +6,29 @@ import Divider from "@material-ui/core/Divider"
 import Todo from './Todo'
 
 function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
-    return (
-        <Paper>
-            <List>
-                {todos.map((todo, i) => (
-                    <>
-                        <Todo
-                            task={todo.task}
-                            key={todo.id}
-                            id={todo.id}
-                            completed={todo.completed}
-                            removeTodo={removeTodo}
-                            toggleTodo={toggleTodo}
-                            editTodo={editTodo}
-                        />
-                        {i < todos.length - 1 && <Divider />}
-                    </>
-                ))}
-            </List>
-        </Paper>
-    )
+    if (todos.length)
+        return (
+            <Paper>
+                <List>
+                    {todos.map((todo, i) => (
+                        <>
+                            <Todo
+                                task={todo.task}
+                                key={todo.id}
+                                id={todo.id}
+                                completed={todo.completed}
+                                removeTodo={removeTodo}
+                                toggleTodo={toggleTodo}
+                                editTodo={editTodo}
+                            />
+                            {i < todos.length - 1 && <Divider />}
+                        </>
+                    ))}
+                </List>
+            </Paper>
+        )
+    else
+        return null
 }
 
 export default TodoList
